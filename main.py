70d449c18fce81082d4dd2572fad7fd1977a519a
xiaoti <xiaoti@ljy.com>
2024-12-06 21:56:13 +0800

    add itermize in extract

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from tqdm import tqdm
 
 
 if __name__ == "__main__":
-    malware_df = Extractor.log2df("mta-2023.log", LABEL.BLACK, replace_src=False)
-    normal_df = Extractor.log2df("campus1.log", LABEL.WHITE, replace_src=True)
-    combiner = Combiner(malware_df, normal_df)
+    mal2normal = {}
+    for malware_log, normal_log in mal2normal.items():
+        malware_df = Extractor.log2df(malware_log, LABEL.BLACK, replace_src=False)
+        normal_df = Extractor.log2df(normal_log, LABEL.WHITE, replace_src=True)
+        combiner = Combiner(malware_df, normal_df)
 
-    combined_df = combiner.get_combined()
-    dfs = utils.split_df_by_time(combined_df, "30min")
-    for df in tqdm(dfs):
-        extractor = Extractor(df=df)
-        extractor.extract()
+        combined_df = combiner.get_combined()
+        dfs = utils.split_df_by_time(combined_df, "10min")
+        for df in tqdm(dfs):
+            extractor = Extractor(df=df)
+            extractor.extract()
